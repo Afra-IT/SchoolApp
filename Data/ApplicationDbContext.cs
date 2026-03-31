67efa057c01753fd94cf62e16c8d79ca1334d913
Sepideh <Sepideh@DESKTOP-7IM3PII>
2026-03-31 21:52:52 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sepideh1.Models;
+
+namespace Sepideh1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } = default!;
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sepideh1.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; } = string.Empty; //todo: test if we remove string empty what will be happen
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
